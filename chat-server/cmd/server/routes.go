fd6c575a3ee12cf7a2ce46f536370a696fbb16e1
harisheoran <harisheoran@protonmail.com>
2024-12-05 15:55:20 +0530

    fix: added bootstrap for responsiveness and fix home route

--- chat-server/cmd/server/routes.go
+++ chat-server/cmd/server/routes.go
@@ -8,7 +8,7 @@ import (
 
 func (app *app) router() http.Handler {
 	mainRouter := mux.NewRouter()
-	mainRouter.HandleFunc("/home", app.homeHandler)
+	mainRouter.HandleFunc("/v1/home", app.homeHandler)
 	mainRouter.HandleFunc("/v1/health", app.healthHandler)
 	mainRouter.HandleFunc("/v1/chat", app.chatHandler)
 
