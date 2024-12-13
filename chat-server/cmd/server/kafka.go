01a44a47987254c13d2ec477500ed068c24ffaa4
harisheoran <harisheoran@protonmail.com>
2024-12-13 13:53:56 +0530

    notfix: kafka is producing each message 2 times

--- chat-server/cmd/server/kafka.go
+++ chat-server/cmd/server/kafka.go
@@ -1,108 +1,6 @@
 package main
 
-import (
-	"context"
-	"crypto/tls"
-	"crypto/x509"
-	"fmt"
-	"os"
-	"time"
-
-	"github.com/joho/godotenv"
-	"github.com/segmentio/kafka-go"
-	"github.com/segmentio/kafka-go/sasl/scram"
-)
-
 /*
-Contains all the kafka code
-*/
-
-var kafkaUrl = "kafka-my-chat-system-particleasw123-2262.c.aivencloud.com:15563"
-
-func (app *app) kafkaInitialize() (*kafka.Dialer, error) {
-	err := godotenv.Load()
-	if err != nil {
-		app.errorlogger.Println("can't read the env files")
-	}
-
-	var username = os.Getenv("KAFKA_USERNAME")
-	var password = os.Getenv("KAFKA_PASSWORD")
-
-	caCert, err := os.ReadFile("ca.pem")
-	if err != nil {
-		app.errorlogger.Println("Failed to read CA certificate file: ", err)
-		return nil, err
-	}
-
-	caCertPool := x509.NewCertPool()
-	ok := caCertPool.AppendCertsFromPEM(caCert)
-	if !ok {
-		app.errorlogger.Println("Failed to parse CA certificate file: ", err)
-		return nil, err
-	}
-
-	tlsConfig := &tls.Config{
-		RootCAs: caCertPool,
-	}
-	scram, err := scram.Mechanism(scram.SHA512, username, password)
-	if err != nil {
-		app.errorlogger.Println("Failed to create scram mechanism: ", err)
-		return nil, err
-	}
-
-	dialer := &kafka.Dialer{
-		Timeout:       10 * time.Second,
-		DualStack:     true,
-		TLS:           tlsConfig,
-		SASLMechanism: scram,
-	}
-
-	return dialer, nil
-}
-
-// create a producer
-// check if there is already a producer then create if not
-func (app *app) createProducer() *kafka.Writer {
-	if producer != nil {
-		return producer
-	}
-
-	dailer, err := app.kafkaInitialize()
-	if err != nil {
-		app.errorlogger.Println("unable to authenticate or initialize with Kafka: ", err)
-	}
-
-	producer := kafka.NewWriter(kafka.WriterConfig{
-		Brokers:  []string{kafkaUrl},
-		Topic:    TOPIC_NAME,
-		Balancer: &kafka.Hash{},
-		Dialer:   dailer,
-	})
-
-	return producer
-}
-
-func (app *app) produceMessage(message string) error {
-	app.infologger.Println("Starting produce message")
-	if producer == nil {
-		app.infologger.Println("Producer founded nil and created")
-		producer = app.createProducer()
-	}
-
-	err := producer.WriteMessages(context.Background(), kafka.Message{
-		Key:   []byte(fmt.Sprintf("message-%s", time.Now().Format("2006-01-02"))),
-		Value: []byte(message),
-	})
-	if err != nil {
-		return err
-	}
-	app.infologger.Println("Message sent: " + message)
-
-	//producer.Close()
-
-	return nil
-}
-
 func (app *app) startKafkaConsumer() {
 	app.infologger.Println("starting the kafka consumer func")
 
@@ -128,3 +26,4 @@ func (app *app) startKafkaConsumer() {
 	}
 
 }
+*/
