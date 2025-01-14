4903c5b7469d4ceea3716c85b2ef01efce6da8e4
harisheoran <harisheoran@protonmail.com>
2025-01-14 14:31:53 +0530

    enable cors

--- chat-server/cmd/server/routes.go
+++ chat-server/cmd/server/routes.go
@@ -29,7 +29,10 @@ func (app *app) router() http.Handler {
 	appRouter.HandleFunc("/online-users/get-count", app.getOnlineUsersCount).Methods("GET")
 
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
+		AllowedOrigins: []string{"*"},
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
 		AllowCredentials: true,
 		AllowedMethods:   []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete, http.MethodOptions},
 		AllowedHeaders:   []string{"Content-Type, Authorization"},
